b8377c0ba351ad2d52e703e6e69200630f8a1eb5
luisRG-L <122104739+luisRG-L@users.noreply.github.com>
2024-10-24 15:31:40 +0200

    Fix images

--- dist/assets/index-tWcI8Vzg.js
+++ dist/assets/index-tWcI8Vzg.js
@@ -27,7 +27,7 @@
             </div>
         </div>
         ${P()}
-    `}const u=e=>document.querySelector(e);function V(e,a){const n=e.toLowerCase().replace(/á/g,"a").replace(/é/g,"e").replace(/í/g,"i").replace(/ó/g,"o").replace(/ú/g,"u"),r=a.toLowerCase().replace(/á/g,"a").replace(/é/g,"e").replace(/í/g,"i").replace(/ó/g,"o").replace(/ú/g,"u");return n.startsWith(r)}const E="https://luisrg-l.github.io/wikimusic/dist",g=e=>`
+    `}const u=e=>document.querySelector(e);function V(e,a){const n=e.toLowerCase().replace(/á/g,"a").replace(/é/g,"e").replace(/í/g,"i").replace(/ó/g,"o").replace(/ú/g,"u"),r=a.toLowerCase().replace(/á/g,"a").replace(/é/g,"e").replace(/í/g,"i").replace(/ó/g,"o").replace(/ú/g,"u");return n.startsWith(r)}const E="https://luisrg-l.github.io/wikimusic/dist/",g=e=>`
 <article class="instrument">
     ${e.img?`<img src="${E}${e.img}" alt="${e.name}">`:""}
     <header class="nohr">
