3622444e0ceff275658e233c07960371f5458e96
luisRG-L <122104739+luisRG-L@users.noreply.github.com>
2025-11-22 11:05:53 +0100

    Update 2.4 (22/11/25) fix

--- dist/assets/index-DMa-4T6f.js
+++ dist/assets/index-DMa-4T6f.js
@@ -27,7 +27,7 @@
             </div>
         </div>
         ${P()}
-    `}const s=e=>document.querySelector(e);function V(e,a){const n=e.toLowerCase().replace(/á/g,"a").replace(/é/g,"e").replace(/í/g,"i").replace(/ó/g,"o").replace(/ú/g,"u"),r=a.toLowerCase().replace(/á/g,"a").replace(/é/g,"e").replace(/í/g,"i").replace(/ó/g,"o").replace(/ú/g,"u");return n.startsWith(r)}const E="../public/instruments/imgs/",g=e=>`
+    `}const s=e=>document.querySelector(e);function V(e,a){const n=e.toLowerCase().replace(/á/g,"a").replace(/é/g,"e").replace(/í/g,"i").replace(/ó/g,"o").replace(/ú/g,"u"),r=a.toLowerCase().replace(/á/g,"a").replace(/é/g,"e").replace(/í/g,"i").replace(/ó/g,"o").replace(/ú/g,"u");return n.startsWith(r)}const E="./public/instruments/imgs/",g=e=>`
 <article class="instrument">
     ${e.img?`<img src="${E}${e.img}" alt="${e.name}">`:""}
     <header class="nohr">
